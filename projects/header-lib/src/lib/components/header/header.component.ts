9479de95aa1140949ea9e231ced8a2b7c59db746
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-02-08 15:25:09 +0300

    in progress

--- projects/header-lib/src/lib/components/header/header.component.ts
+++ projects/header-lib/src/lib/components/header/header.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'next-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Input() env: string;
+  @Input() role: string;
 
   constructor() { }
 
--- projects/header-lib/src/lib/header-lib.module.ts
+++ projects/header-lib/src/lib/header-lib.module.ts
@@ -1,3 +1,4 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderLibComponent } from './header-lib.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -5,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [HeaderLibComponent, HeaderComponent],
   imports: [
+    BrowserModule,
   ],
   exports: [
     HeaderLibComponent,

0587947030500004480a44033f016c641b11c7bb
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-02-18 17:36:39 +0300

    add tests

--- projects/header-lib/src/lib/components/header/header.component.spec.ts
+++ projects/header-lib/src/lib/components/header/header.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let spyOnClick: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,34 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.env = 'dev';
+    component.role = 'NEXT - NIBR';
+    component.descr = 'External Science';
+    spyOnClick = spyOn(component, 'onProjectNameClick');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event by click on project name', () => {
+    component.onProjectNameClick();
+    expect(spyOnClick).toHaveBeenCalled();
+  });
+
+  it('should show dev environment', () => {
+    const element = fixture.debugElement.query(By.css('.next-header__env'));
+    expect(element.nativeElement.textContent).toContain('dev');
+  });
+
+  it('should show name', () => {
+    const element = fixture.debugElement.query(By.css('.next-header__name'));
+    expect(element.nativeElement.textContent).toContain('NEXT - NIBR');
+  });
+
+  it('should show descr', () => {
+    const element = fixture.debugElement.query(By.css('.next-header__descr'));
+    expect(element.nativeElement.textContent).toContain('External Science');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'projects/header-lib/src/public_api';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
     }).compileComponents();
   }));
@@ -16,13 +18,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'header-lib-app'`, () => {
+  xit(`should have as title 'header-lib-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('header-lib-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  xit('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
